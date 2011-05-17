bf776cba533f8b070382f7276a1fba0693c8ab14
ricenzhang <ricenzhang@f3c43967-037a-eae0-30b1-3b156cf2a74a>
2011-05-17 04:10:17 +0000

    # add xcode.bat to xcode tools to assign path.

---
+++ common/xcode.bat
@@ -0,0 +1,45 @@
+rem @echo off
+
+rem this parameter must change to svnpath(xcode.bat path).
+set svn=C:\tmp\common
+
+set xrc=c:\qadguicli
+set xcode=C:\qadguicli\proedit
+set src=C:\qadguicli\garbage\src
+set cim=C:\qadguicli\garbage\cim
+del %src%\*.* /s/q/f/a
+del %xrc%\xrc\*.* /s/q/f/a
+rem rd %xrc%\xrc
+mkdir %xrc%\garbage\src
+mkdir /d %xrc%\xrc
+
+rem copy src to srcdir
+mkdir %src%
+copy /y %svn%\xxbdpro\xxbdpro.p %src%\
+copy /y %svn%\xxcomp\xx*.i %src%\
+copy /y %svn%\xxcomp\xxc.p %src%\
+copy /y %svn%\xxcomp\xxcomp.p %src%\
+copy /y %svn%\xxcomp\xxcompil.p %src%\
+copy /y %svn%\xxmsgiq.p %src%\
+copy /y %svn%\xxlbliq\*.p %src%\
+copy /y %svn%\xxgettab\*.* %src%\
+
+rem Xcode.
+cd /d %src%
+%xcode%\xcode -d %xrc%\xrc xx*.p
+%xcode%\xcode -d %xrc%\xrc xx*.i
+%xcode%\xcode -d %xrc%\xrc xx*.v
+
+copy %cim%\*.cim %xrc%\xrc\c.cim
+
+rem convert.
+mkdir %xrc%\gui
+if %1=="A" goto exitme
+echo srcSearchPath=%src%> %xrc%\convert.tmp
+echo targetDir=%xrc%\gui>> %xrc%\convert.tmp
+echo workDir=%xrc%>> %xrc%\convert.tmp
+%xcode%\convert.exe -r -ini=%xrc%\convert.tmp %src%\xx*.p>%xrc%\convert.log
+%xcode%\convert.exe -r -ini=%xrc%\convert.tmp %src%\xx*.i>>%xrc%\convert.log
+rem del %xrc%\convert.tmp /s/q/f/a
+
+:exitme
