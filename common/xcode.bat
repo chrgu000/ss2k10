4f5d211c4e9b00b158f4431ca24cfd5deab868ab
ricenzhang <ricenzhang@f3c43967-037a-eae0-30b1-3b156cf2a74a>
2011-07-19 09:33:17 +0000

    # update xcode.bat for batch install program.

--- common/xcode.bat
+++ common/xcode.bat
@@ -8,10 +8,12 @@ set xcode=C:\qadguicli\proedit
 set src=C:\qadguicli\garbage\src
 set cim=C:\qadguicli\garbage\cim
 del %src%\*.* /s/q/f/a
+del %cim%\*.* /s/q/f/a
 del %xrc%\xrc\*.* /s/q/f/a
 rem rd %xrc%\xrc
-mkdir %xrc%\garbage\src
-mkdir /d %xrc%\xrc
+mkdir %src%
+mkdir %xrc%\xrc
+mkdir %cim%
 
 rem copy src to srcdir
 mkdir %src%
@@ -27,9 +29,10 @@ copy /y %svn%\xxlbliq\xx*.? %src%\
 copy /y %svn%\xxmecm\xx*.? %src%\
 copy /y %svn%\xxmeiq\xx*.? %src%\
 copy /y %svn%\xxmgflh1\xx*.? %src%\
+copy /y %svn%\xxmgflh1\l.cim %cim%\xxmgflh1.cim
 copy /y %svn%\xxnrgnmt\xx*.? %src%\
 copy /y %svn%\xxvifile\xx*.? %src%\
-copy /y %svn%\xxvifile\*.cim %cim%\
+copy /y %svn%\xxvifile\p.cim %cim%\xxvifile.cim
 
 copy /y %svn%\xxmsgiq.p %src%\
 copy /y %svn%\xxmsgmt.p %src%\
@@ -40,7 +43,7 @@ cd /d %src%
 %xcode%\xcode -d %xrc%\xrc xx*.i
 %xcode%\xcode -d %xrc%\xrc xx*.v
 
-copy %cim%\*.cim %xrc%\xrc\c.cim
+copy %cim%\*.cim %xrc%\xrc\p.cim
 
 rem convert.
 mkdir %xrc%\gui
