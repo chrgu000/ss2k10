cb7952d02cda35ebf6c67a642c0da4ce0c35792e
r7733251 <r7733251@f3c43967-037a-eae0-30b1-3b156cf2a74a>
2012-12-13 05:24:55 +0000

    # fix bug . delete temp file.

--- common/xxbdpro/remove.i.cmd
+++ common/xxbdpro/remove.i.cmd
@@ -3,3 +3,4 @@ del D:\ss\trunk\common\xxbdpro\xxqaddom.i /s/q/f/a
 del D:\ss\trunk\common\xxbdpro\xxusrwdom.i /s/q/f/a
 del D:\ss\trunk\common\xxbdpro\xxbdldom.i /s/q/f/a
 del D:\ss\trunk\common\xxbdpro\xxbdlddom.i /s/q/f/a
+del D:\ss\trunk\common\xxbdpro\xxmfnsq.i /s/q/f/a
