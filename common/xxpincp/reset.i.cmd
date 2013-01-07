926c406cffdf67df3cbc8c51737b483f5121ed97
r7733251 <r7733251@f3c43967-037a-eae0-30b1-3b156cf2a74a>
2013-01-07 07:25:42 +0000

    # rest xxpincp.i.

---
+++ common/xxpincp/reset.i.cmd
@@ -0,0 +1,4 @@
+cd /d D:\ss\trunk\common\xxpincp
+echo /*V8:ConvertMode=Maintenance                                                 */> xxusrwdom.i
+echo /*V8:ConvertMode=Maintenance                                                 */> xxusrwdom1.i
+echo /*V8:ConvertMode=Maintenance                                                 */> xxand.i
